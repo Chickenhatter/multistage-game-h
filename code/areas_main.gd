4352a7682b86601da1cb2ec8e999dea3b275a5b7
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-03-31 19:50:41 +1300

    1.5hr I am trying to make thing recognise when both char in box and spirte in box and make image stick to box

--- code/areas_main.gd
+++ code/areas_main.gd
@@ -1,6 +1,6 @@
-extends Node
-var level = 1
-var boxx = -100
+extends Node2D
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,3 +9,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_one_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = 100
+
+func _on_one_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = -100
--- other scenes/invis_choose.gd
+++ other scenes/invis_choose.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 var v = 0
 var m = 0
+var mov = false
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -17,8 +18,19 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
-	$"../AnimatedSprite2D".position.y = sin(v)
-	if v > m:
-		$"../AnimatedSprite2D".position.x -= 1
-	if v < m:
-		$"../AnimatedSprite2D".position.x += 1
+	$"../AnimatedSprite2D".position.y = 40*sin(0.02*v)
+	mov = false
+	if (completion.boxx == -100):
+		if (v > m):
+			if ((v+3)>m):
+				if ((v-3)>m):
+					$"../AnimatedSprite2D".position.x -= 3
+					mov = true
+		if v < m:
+			if ((v+3)<m):
+				if ((v-3)<m):
+					$"../AnimatedSprite2D".position.x += 3
+					mov = true
+	if mov == false:
+		print('g')
+	print('v')
