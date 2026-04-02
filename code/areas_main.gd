27aa81068fcb114fa7da4f3fd851b30fa9637ee4
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-04-02 22:09:21 +1300

    2.5hr I have done 5 of the boxes, also added sin image so I can follow it
    Teachers said math is important and I didn't expect to be using sin and calculating abcd

--- code/areas_main.gd
+++ code/areas_main.gd
@@ -18,3 +18,33 @@ func _on_one_body_entered(body: Node2D) -> void:
 func _on_one_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		completion.boxx = -100
+
+
+func _on_two_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = 168
+
+
+func _on_two_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = -100
+
+
+func _on_three_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = 249
+
+
+func _on_three_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = -100
+
+
+func _on_four_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = 328
+
+
+func _on_four_body_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		completion.boxx = -100
--- other scenes/invis_choose.gd
+++ other scenes/invis_choose.gd
@@ -9,23 +9,24 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-
+	var motion = Vector2.ZERO
 	v = $"../AnimatedSprite2D".position.x
 	m = $".".position.x
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	var direction := Input.get_axis("ui_a", "ui_d")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if m > 4:
+		if Input.is_action_pressed('ui_a'):
+			motion.x -= 1
+	if m < 500:
+		if Input.is_action_pressed('ui_d'):
+			motion.x += 1
+	velocity = motion * SPEED
 	move_and_slide()
 	$"../AnimatedSprite2D".position.y = 40*sin(0.02*v)
 	mov = false
 	$"../hwd".position.x = $"../AnimatedSprite2D".position.x 
 	if (completion.boxx != -100) and (furtogether == true):
 		$"../AnimatedSprite2D".position.x = completion.boxx
-		print(completion.boxx)
 	elif together == false:
 		if (v > m):
 			if ((v+3)>m):
