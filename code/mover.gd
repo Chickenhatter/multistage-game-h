49874a4ff56c4092e2567db54f402ecdc4e9a92d
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-08 17:08:35 +1200

    8hr 30m I made level 6 and I was trying to make the platform push

--- code/mover.gd
+++ code/mover.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	var movement = Vector2.ZERO
-	v = sin(a/1)
+	v = sin(a/3)
 	print(v)
 	a += 0.1
 	if $".".position.y >= 0:
--- level/character_body_2dwmover.gd
+++ level/character_body_2dwmover.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
-
+var lekel = false
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if lekel == true:
+		var movement = Vector2.ZERO
+		movement.y -= 1
+		velocity += movement * 1
+	if (not is_on_floor()) and (lekel == false) :
 		velocity += (get_gravity() * delta * 0.7)
-	if Input.is_action_just_pressed("ui_w") and is_on_floor():
+	if Input.is_action_just_pressed("ui_w") and ((is_on_floor()) or (lekel == true)):
 		velocity.y = JUMP_VELOCITY
 	var direction := Input.get_axis("ui_a", "ui_d")
 	if direction:
@@ -25,5 +29,14 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play('standing')
 	move_and_slide()
 
-func _on_anti_mover_char_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+
+
+
+func _on_anti_mover_char_body_entered(body: Node2D) -> void:
+	if body.name == 'CharacterBody2D':
+		lekel = true
+
+
+func _on_anti_mover_char_body_exited(body: Node2D) -> void:
+	if body.name == 'CharacterBody2D':
+		lekel = false
