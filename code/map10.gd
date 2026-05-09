2377362cc01ae603c6dc18feff0abafe881ebec7
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-09 14:58:23 +1200

    11.5hr finished 9 and started work on 10

--- code/map10.gd
+++ code/map10.gd
@@ -1,11 +1,36 @@
 extends Node2D
-
-
+var unsee = false
+var pathfin = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	completion.warn = 0
+	$"../Node2D2/CharacterBody2D/Unsee".self_modulate.a = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if unsee == false:
+		if $"../Node2D2/CharacterBody2D/Unsee".self_modulate.a > 0:
+			$"../Node2D2/CharacterBody2D/Unsee".self_modulate.a -= 0.06
+	if unsee == true:
+		$"../Node2D2/CharacterBody2D/Unsee".self_modulate.a += 0.16
+	
+
+func _on_death_plane_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		unsee = true
+
+
+func _on_finish_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		unsee = true
+		await get_tree().create_timer(2.0).timeout
+		if completion.level <= 9:
+			completion.level = 10
+			completion.cap = 776
+		get_tree().change_scene_to_file('res://other scenes/start.tscn')
+
+
+func _on_trudeth_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file('res://other scenes/levelchoose.tscn')
